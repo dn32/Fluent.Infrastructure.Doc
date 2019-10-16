8e90184dbcd3d8be7c34c467d6a0d2f3855ef5f7
Marcelo de casa <dn@dn32.com.br>
2019-10-15 21:42:50 -0300

    Exemplo de agregação

---
+++ Samples/07-Aggregation/Entities/Company.cs
@@ -0,0 +1,13 @@
+﻿using Fluent.Architecture.Core.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Composition.Entities
+{
+    public class Company : FluentEntity
+    {
+        [Key]
+        public int Code { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Samples/07-Aggregation/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using Fluent.Architecture.Core.Attributes;
+using Fluent.Architecture.Core.Models;
+using Fluent.Architecture.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Composition.Entities
+{
+    public class User : FluentEntity, IFluentInclusionEntity
+    {
+        [Key]
+        public int Code { get; set; }
+
+        [FluentAggregation(LocalKey = nameof(CompanyCode), ExternalKey = nameof(Entities.Company.Code))]
+        [ForeignKey(nameof(CompanyCode))]
+        public Company Company { get; set; }
+
+        public int CompanyCode { get; set; }
+
+        public string Name { get; set; }
+
+        public string[] InclusionsForList => new[] { nameof(Company) };
+
+        public string[] InclusionsForOne => new[] { nameof(Company) };
+    }
+}
