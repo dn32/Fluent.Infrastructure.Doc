846809f3a15d979896115470b796bad264aa21c3
Marcelo de casa <dn@dn32.com.br>
2019-10-18 11:53:50 -0300

    Exemplo de sql

---
+++ Samples/14-Sql/scope/User.cs
@@ -0,0 +1,27 @@
+﻿using Fluent.Architecture.Attributes;
+using Fluent.Architecture.Core.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleHelloWorld
+{
+    /* 3. Example entity */
+    public class User : FluentEntity
+    {
+        [Key]
+        public long Code { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public PernonType PernonType { get; set; }
+    }
+
+    [FluentEnumValueForSetNull(value: 0)]
+    public enum PernonType
+    {
+        None = 0,
+        LegalPerson = 1,
+        PhysicalPerson = 2
+    }
+}
---
+++ Samples/14-Sql/scope/UserService.cs
@@ -0,0 +1,24 @@
+﻿using Fluent.Architecture.Core.Models;
+using Fluent.Architecture.EntityFramework.Specifications;
+using Fluent.Architecture.Extensions;
+using Fluent.Architecture.Interfaces;
+using Fluent.Architecture.Services;
+using SimpleHelloWorld;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace sql.scope
+{
+    public class UserService : FluentService<User>
+    {
+        public override async Task<List<User>> ListAsync(IFluentSpecification spec, FluentPagination pagination = null)
+        {
+            var tableName = typeof(User).GetTableName();
+            var collumName = typeof(User).GetProperty("Code").GetColumnName();
+            var sql = $"select * from {tableName} where {collumName} = {{0}}";
+
+            var newSpec = CreateSpec<FluentSqlSpec<User>>().SetParameter(sql, 45);
+            return await base.ListAsync(newSpec, pagination);
+        }
+    }
+}
